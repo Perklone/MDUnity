c851b92e4a61f06fd15ec80da01b5d1e3c352d92
Rizky Maulana <mrizky9601@gmail.com>
2023-05-19 18:50:57 +0700

    Material Changer with temporary button

--- Assets/Scripts/ModifyHome.cs
+++ Assets/Scripts/ModifyHome.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 public class ModifyHome : MonoBehaviour
@@ -8,14 +9,18 @@ public class ModifyHome : MonoBehaviour
     [SerializeField] private Slider redSlider;
     [SerializeField] private Slider greenSlider;
     [SerializeField] private Slider blueSlider;
+    [SerializeField] private TMP_Text indexText;
 
     public Renderer _houseRenderer;
     private Material[] houseMaterial;
-    public Material altMaterial;
+    public Material[] altMaterial;
+
+    public int materialIndex;
     // public GameObject House;
     void Start()
     {
         // _houseRenderer = House.GetComponent<Renderer>();
+        materialIndex = 0;
         houseMaterial = _houseRenderer.materials;
         Debug.Log($"{houseMaterial[0]}, {houseMaterial[1]}");
         Color32 currentColor = houseMaterial[1].GetColor("_Color");
@@ -50,13 +55,39 @@ public class ModifyHome : MonoBehaviour
         _houseRenderer.materials[1].SetColor("_Color",newColor);   
     }
 
-    public void OnButtonClicked()
+    public void onLeftButtonClicked()
     {
         Material[] x = _houseRenderer.materials;
         Color newColor = new Color(redSlider.value, greenSlider.value, blueSlider.value);
-        altMaterial.SetColor("_Color",newColor);
-        x[1] = altMaterial;
-        Debug.Log("OnButtonClicked Activated");
+        if (materialIndex == 0)
+        {
+            materialIndex = altMaterial.Length-1;
+        }
+        else
+        {
+            materialIndex--;
+        }
+        altMaterial[materialIndex].SetColor("_Color",newColor);
+        x[1] = altMaterial[materialIndex];
         _houseRenderer.materials = x;
+        indexText.text = $"{materialIndex + 1}";
+    }
+    
+    public void onRightButtonClicked()
+    {
+        Material[] x = _houseRenderer.materials;
+        Color newColor = new Color(redSlider.value, greenSlider.value, blueSlider.value);
+        if (materialIndex == altMaterial.Length-1)
+        {
+            materialIndex = 0;
+        }
+        else
+        {
+            materialIndex++;
+        }
+        altMaterial[materialIndex].SetColor("_Color",newColor);
+        x[1] = altMaterial[materialIndex];
+        _houseRenderer.materials = x;
+        indexText.text = $"{materialIndex + 1}";
     }
 }
