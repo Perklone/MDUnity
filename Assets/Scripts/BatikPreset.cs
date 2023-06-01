fcfd4c0c399b5b8507861be394997b2736b7ce7c
Rizky Maulana <mrizky9601@gmail.com>
2023-06-01 17:18:08 +0700

    Created Interfaces and Preset for Rumah Batik

---
+++ Assets/Scripts/BatikPreset.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BatikPreset : MonoBehaviour, IPresetMenu
+{
+    public TMP_Text indexText;
+    public Renderer _houseRenderer;
+    public Material[] altMaterial;
+    private Material[] houseMaterial;
+
+    public int presetIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        houseMaterial = _houseRenderer.materials;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LeftButtonClicked()
+    {
+        Material[] tempMaterials = _houseRenderer.materials;
+        if (presetIndex == 0)
+        {
+            presetIndex = altMaterial.Length-1;
+        }
+        else
+        {
+            presetIndex--;
+        }
+
+        tempMaterials[1] = altMaterial[presetIndex];
+        _houseRenderer.materials = tempMaterials;
+        UpdateIndexLabel();
+    }
+
+    public void RightButtonClicked()
+    {
+        Material[] tempMaterials = _houseRenderer.materials;
+        if (presetIndex == altMaterial.Length-1)
+        {
+            presetIndex = 0;
+        }
+        else
+        {
+            presetIndex++;
+        }
+
+        tempMaterials[1] = altMaterial[presetIndex];
+        _houseRenderer.materials = tempMaterials;
+        UpdateIndexLabel();
+    }
+
+    public void UpdateIndexLabel()
+    {
+        indexText.text = $"{presetIndex + 1}";
+    }
+}
